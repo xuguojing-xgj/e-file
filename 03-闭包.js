b29ef5a61a3c6b87cfae9a431d1b615ff38cfde2
xuguojing-xgj <2313493661@qq.com>
2022-06-02 15:33:32 +0800

    闭包,data为什么是个函数

--- 03-闭包.js
+++ 03-闭包.js
@@ -4,7 +4,7 @@
 //  在函数外面访问函数里面的变量
 
 function foo() {
-    let a = 1
+    let a = 'a'
     function bar() {
         console.log(a)
     }
@@ -12,3 +12,34 @@ function foo() {
 }
 let baz = foo()
 baz()
+
+let containerFn = function fn1() {
+    let num = 0
+    function changeBy(val) {
+        num += val
+    }
+
+    return {
+        add: function () {
+            changeBy(1)
+        },
+        reduce: function () {
+            changeBy(-1)
+        },
+        value: function () {
+            return num
+        }
+    }
+}
+let content1 = containerFn()
+let content2 = containerFn()
+
+content1.add()
+content1.add()
+content2.add()
+
+console.log(content2.value()); // 1
+console.log(content1.value()); // 2
+
+// 闭包 都拥有自己独立的 作用域
+// 面向对象的编程 -> 数据的隐层 和 封装
