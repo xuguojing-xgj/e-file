c041bf5e4cd18b827aa36a7244641708ca5051e3
xuguojing-xgj <2313493661@qq.com>
2022-06-02 14:39:54 +0800

    闭包注释

--- 03-闭包.js
+++ 03-闭包.js
@@ -1,9 +1,14 @@
+//  闭包的作用 :
+// 延长变量的生命周期 创建私有环境
+//  什么是闭包?
+//  在函数外面访问函数里面的变量
+
 function foo() {
     let a = 1
-      function bar() {
-         console.log(a)
-      }
-      return bar
-  }
- let baz = foo()
- baz()
+    function bar() {
+        console.log(a)
+    }
+    return bar
+}
+let baz = foo()
+baz()
