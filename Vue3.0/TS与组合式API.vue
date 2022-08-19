c7090e7f2d7f787e2e3988006322bac6680c92cf
xuguojing-xgj <2313493661@qq.com>
2022-08-19 23:02:10 +0800

    ts vue3 组合式api

---
+++ Vue3.0/TS与组合式API.vue
@@ -0,0 +1,18 @@
+<!--  为组件的props 标注类型 -->
+<!-- 使用 <script setup> 时 defineProps() 宏函数支持从它的参数类型中推到类型 : -->
+
+<script setup lang="ts">
+// 这被称之为“运行时声明”，因为传递给 defineProps() 的参数会作为运行时的 props 选项使用。
+const props = defineProps({
+    foo: { type: String, required: true },
+    bar: number
+})
+props.foo // string
+props.bar // number | undefined
+
+// 通过泛型参数来定义 props 的类型通常更直接：
+const props = defineProps<{
+    foo: String,
+    bar?: number
+}>()
+</script>
