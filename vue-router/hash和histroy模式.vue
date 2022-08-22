59075908d15bbb5bff865eab2cad34cd4cdfe96a
xuguojing-xgj <2313493661@qq.com>
2022-08-22 21:16:19 +0800

    hash history 模式 简介

---
+++ vue-router/hash和histroy模式.vue
@@ -0,0 +1,15 @@
+<!-- hash 和 history  -->
+
+<!-- 区别在于 url 形式 -->
+hsah : 'http://xx.com/#/about'
+history : 'http://xx.com/about'
+
+在 vue-router 4.x 版本中 设置模式发生变化
+const router = createRouter({
+    history: createWebHashHistory(),  // hash模式
+    history: createWebHistory(), // history 模式
+    history: createMemoryHistory(), // menory 模式
+})
+
+<!-- 使用 -->
+<router-link to="/about">Go To About </router-link>
