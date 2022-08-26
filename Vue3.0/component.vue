0c25cb6f2e4ab16552bf53ce0864fcbcaab24ad0
Violet 02 <79581129+xuguojing-xgj@users.noreply.github.com>
2022-08-26 16:57:40 +0800

    Update component.vue

--- Vue3.0/component.vue
+++ Vue3.0/component.vue
@@ -1,1 +1,14 @@
 // component 特殊元素 的组件 :is=""  代表动态绑定的组件
+
+// 基于组合式 api setup 使用 
+<script setup>
+import Foo from './Foo.vue'
+import Bar from './Bar.vue'
+</script>
+
+<template>
+  <component :is="Math.random() > 0.5 ? Foo : Bar" />
+</template>
+
+// 渲染 HTML 元素：
+<component :is="href ? 'a' : 'span'"></component>
