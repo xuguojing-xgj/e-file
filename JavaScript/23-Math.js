a8ac61d7987ab36be1884cfd3d1734a98ea7a20a
violet02 <xuguojing77@163.com>
2022-09-08 15:18:49 +0800

    Math.acosh()方法

--- JavaScript/23-Math.js
+++ JavaScript/23-Math.js
@@ -9,9 +9,17 @@ console.log(res);
 
 /**
  * Math.acos(x) 方法 以 -1 到 1 的一个数为参数 
- * 返回一个 0 到 pi(弧度的数值) 
+ * 返回一个给定 0 到 pi(弧度的数值) 
  * x 值小于 -1 或者是 大于 1  acos 返回NAN
  */
 
 let res1 = Math.acos(0.2)
 console.log(res1);
+
+/**
+ * Math.acosh(x)  方法返回一个给定数的反双曲余弦值, 如果x小于1 返回NAN
+ * x 是 1 返回 0
+ */
+
+let res2 = Math.acosh(99)
+console.log(res2);
