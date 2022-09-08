741f09f3bb05d60906ccf21f91798634922f6f0c
violet02 <xuguojing77@163.com>
2022-09-08 15:25:43 +0800

    update file while

--- JavaScript/22-while用法.js
+++ JavaScript/22-while用法.js
@@ -1,18 +1,21 @@
 // while 循环
-cars=["BMW","Volvo","Saab","Ford"];
-var i=0;
-while (cars[i]){
+cars = ["BMW", "Volvo", "Saab", "Ford"];
+
+var i = 0;
+
+while (cars[i]) {
 	document.write(cars[i] + "<br>");
 	i++;
 }
 
 
-function myFunction(){
-	var x="",i=0;
-	while (i <= 10 ){
-		x=x + "该数字为 " + i + "<br>";
+function myFunction() {
+	var x = "", i = 0;
+	while (i <= 10) {
+		x = x + "该数字为 " + i + "<br>";
 		i++;
 	}
-	document.getElementById("demo").innerHTML=x;
+	document.getElementById("demo").innerHTML = x;
 }
+
 myFunction()
