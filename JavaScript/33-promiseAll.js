477f723fafee2e5d329dfccf85fc76bb2d2f0c01
xuguojing-xgj <2313493661@qq.com>
2022-11-19 23:22:06 +0800

    update files

---
+++ JavaScript/33-promiseAll.js
@@ -0,0 +1,14 @@
+const promise1 = Promise.resolve('190')
+
+const promise2 = 42
+
+// const promise2_1 = Promise.reject('reject')
+const promise3 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve('100', 'foo')
+    }, 2000)
+})
+
+Promise.all([promise1, promise2, promise3]).then((vals) => {
+    console.log(vals)
+})
