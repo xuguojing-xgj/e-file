f6ce42c332c99928636ed42850d9a88d772253f9
xuguojing-xgj <2313493661@qq.com>
2022-12-24 21:53:45 +0800

    update file

--- RegExp(re)/01-正则基础.js
+++ RegExp(re)/01-正则基础.js
@@ -2,5 +2,15 @@
 
 // 捕获 : 把字符串中符合规则的部分捕获到(exec / match / test)
 
+/**
+ * test() 方法 返回 true 和 false
+ */
 let re = /^\d+$/ // 只能是数字一到多位 ^caret符
 console.log(re.test('100')); // 返回 true OR false
+
+
+
+/**
+ * exec() 方法 指定字符串中搜索 返回一个数组或null 
+ */
+
