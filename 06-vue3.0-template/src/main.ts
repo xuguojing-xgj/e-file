fa7863e13a86949d162288f4be63306abfbe159a
xuguojing-xgj <xuguojing77@163.com>
2024-08-25 22:47:27 +0800

    vue3.0-template update

--- 06-vue3.0-template/src/main.ts
+++ 06-vue3.0-template/src/main.ts
@@ -19,9 +19,12 @@ const pinia = createPinia();
 import router from './router';
 
 
+// 
+import { MotionPlugin } from "@vueuse/motion";
+
 const app = createApp(App);
 
-
+app.use(MotionPlugin)
 app.use(router)
 app.use(pinia)
 app.use(ElementPlus)
--- 06-vue3.0-template/src/views/login/login.vue
+++ 06-vue3.0-template/src/views/login/login.vue
@@ -1,6 +1,9 @@
 <script setup lang="ts">
-import { ref } from "vue";
-
+import Motion from "./utils/motion";
+import { ref, reactive, toRaw, onMounted, onBeforeUnmount } from "vue";
+import { bg, avatar, illustration } from "./utils/static";
+// import dayIcon from "@/assets/svg/day.svg";
+// import darkIcon from "@/assets/svg/dark.svg";
 const dataTheme = ref("");
 
 const dataThemeChange = () => {};
@@ -12,81 +15,71 @@ const ruleForm = ref({
   password: "",
 });
 
-const loginRules = [{}];
+const loginRules = ref([{}]);
 
 const onLogin = () => {};
 </script>
 
 <template>
   <div class="select-none">
-    <img src="" class="wave" />
+    <img :src="bg" class="wave" />
     <div class="flex-c absolute right-5 top-3">
       <!-- 主题 -->
-      <el-switch
-        v-model="dataTheme"
-        inline-prompt
-        active-icon="dayIcon"
-        inactive-icon="darkIcon"
-        @change="dataThemeChange"
-      />
+      <!-- <el-switch v-model="dataTheme" inline-prompt @change="dataThemeChange" /> -->
     </div>
     <div class="login-container">
       <div class="img">
-        <!-- <component is="toRaw(illustration)" /> -->
+        <img :src="illustration" alt="" />
       </div>
       <div class="login-box">
         <div class="login-form">
           <!-- <avatar class="avatar" /> -->
-          <!-- <Motion>
+          <img class="avatar" :src="avatar" alt="" />
+          <Motion>
             <h2 class="outline-none">{{ title }}</h2>
-          </Motion> -->
+          </Motion>
 
-          <el-form
-            ref="ruleFormRef"
-            :model="ruleForm"
-            :rules="loginRules"
-            size="large"
-          >
-            <!-- <Motion :delay="100"> -->
-            <el-form-item
-              :rules="[
-                {
-                  required: true,
-                  message: '请输入账号',
-                  trigger: 'blur',
-                },
-              ]"
-              prop="username"
-            >
-              <el-input
-                v-model="ruleForm.username"
-                clearable
-                placeholder="账号"
-              />
-            </el-form-item>
-            <!-- </Motion> -->
+          <el-form ref="ruleFormRef" :model="ruleForm" size="large">
+            <Motion :delay="100">
+              <el-form-item
+                :rules="[
+                  {
+                    required: true,
+                    message: '请输入账号',
+                    trigger: 'blur',
+                  },
+                ]"
+                prop="username"
+              >
+                <el-input
+                  v-model="ruleForm.username"
+                  clearable
+                  placeholder="账号"
+                />
+              </el-form-item>
+            </Motion>
 
-            <!-- <Motion :delay="150"> -->
-            <el-form-item prop="password">
-              <el-input
-                v-model="ruleForm.password"
-                clearable
-                show-password
-                placeholder="密码"
-              />
-            </el-form-item>
-            <!-- </Motion> -->
+            <Motion :delay="150">
+              <el-form-item prop="password">
+                <el-input
+                  v-model="ruleForm.password"
+                  clearable
+                  show-password
+                  placeholder="密码"
+                />
+              </el-form-item>
+            </Motion>
 
-            <!-- <Motion :delay="250"> -->
-            <el-button
-              class="w-full mt-4"
-              size="default"
-              type="primary"
-              @click="onLogin"
-            >
-              登录
-            </el-button>
-            <!-- </Motion> -->
+            <Motion :delay="250">
+              <el-button
+                class="w-full mt-4"
+                size="default"
+                type="primary"
+                @click="onLogin"
+              >
+                登录
+              </el-button>
+            </Motion>
           </el-form>
         </div>
       </div>
@@ -97,4 +90,8 @@ const onLogin = () => {};
 <style scoped>
 @import url("@/styles/login.css");
 </style>
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+:deep(.el-input-group__append, .el-input-group__prepend) {
+  padding: 0;
+}
+</style>
