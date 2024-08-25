f5af38095c35b2819651cbdc9b64231a3a9e81ce
xuguojing-xgj <xuguojing77@163.com>
2024-08-25 22:04:54 +0800

    udpate

---
+++ 06-vue3.0-template/src/main.ts
@@ -0,0 +1,28 @@
+import { createApp } from 'vue';
+import App from './App.vue';
+// 引入重置样式
+import "./styles/reset.scss";
+
+// element-plus
+import ElementPlus from 'element-plus';
+import "element-plus/dist/index.css";
+
+
+
+// pinia
+import { createPinia } from 'pinia';
+const pinia = createPinia();
+
+
+// vue-router
+
+import router from './router';
+
+
+const app = createApp(App);
+
+
+app.use(router)
+app.use(pinia)
+app.use(ElementPlus)
+app.mount('#app')
